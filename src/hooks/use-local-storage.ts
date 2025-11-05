cdd52092a42ee14e91338cb6ec389a4030907bf6
Edmilson Rodrigues do Nascimento Junior <ernj@cin.ufpe.br>
2025-11-05 15:12:02 +0000

    Try fixing this error: `Console Error: Maximum update depth exceeded. Th

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -2,22 +2,23 @@
 
 import { useState, useEffect, useCallback } from 'react';
 
-function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((val: T) => T)) => void] {
-  const readValue = useCallback((): T => {
+const readValueFromLocalStorage = <T>(key: string, initialValue: T): T => {
     if (typeof window === 'undefined') {
-      return initialValue;
+        return initialValue;
     }
 
     try {
-      const item = window.localStorage.getItem(key);
-      return item ? (JSON.parse(item) as T) : initialValue;
+        const item = window.localStorage.getItem(key);
+        return item ? (JSON.parse(item) as T) : initialValue;
     } catch (error) {
-      console.warn(`Error reading localStorage key “${key}”:`, error);
-      return initialValue;
+        console.warn(`Error reading localStorage key “${key}”:`, error);
+        return initialValue;
     }
-  }, [initialValue, key]);
+}
 
-  const [storedValue, setStoredValue] = useState<T>(readValue);
+function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((val: T) => T)) => void] {
+  
+  const [storedValue, setStoredValue] = useState<T>(() => readValueFromLocalStorage(key, initialValue));
 
   const setValue = (value: T | ((val: T) => T)) => {
     if (typeof window === 'undefined') {
@@ -34,8 +35,9 @@ function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((val
   };
   
   useEffect(() => {
-    setStoredValue(readValue());
-  }, [readValue]);
+    setStoredValue(readValueFromLocalStorage(key, initialValue));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return [storedValue, setValue];
 }

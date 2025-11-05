90f50bf22ce3ea7b0092c9d97e2b079fa1f7683f
Edmilson Rodrigues do Nascimento Junior <ernj@cin.ufpe.br>
2025-11-05 18:48:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7861892440-bca98",
+  "appId": "1:506409039421:web:fbd1b9172674c46c605c54",
+  "apiKey": "AIzaSyDltFIIZwsLX97imLogPdkUyZ0oB7OKTKE",
+  "authDomain": "studio-7861892440-bca98.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "506409039421"
+};

4746a67c7d4f2f9f22300b32f112609094d92a0b
Edmilson Rodrigues do Nascimento Junior <ernj@cin.ufpe.br>
2025-11-12 11:20:40 +0000

    Aumente o tamanho da fonte do texto no rodapé

--- src/components/app/footer.tsx
+++ src/components/app/footer.tsx
@@ -5,12 +5,12 @@ export function Footer() {
     <footer className="border-t bg-background">
       <div className="container flex h-24 items-center justify-center">
         <div className="flex items-center gap-2 text-muted-foreground">
-          <span className="text-sm">Um produto</span>
+          <span className="text-base">Um produto</span>
           <Link
             href="https://www.cin.ufpe.br/~v-lab/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-sm font-semibold text-foreground hover:underline"
+            className="text-base font-semibold text-foreground hover:underline"
           >
             V-Lab
           </Link>
